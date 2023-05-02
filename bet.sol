801d391684f15885e59666845b22560a83843781
Po-Han Chen <pohchen@cs.nctu.edu.tw>
2023-05-02 21:15:01 +0800

    Update bet.sol

--- bet.sol
+++ bet.sol
@@ -1,20 +1,24 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Betting {
-    mapping(address => uint256) public balances;
-    address public owner = 0xfF2c5C74B24E7b6B5A7cd20E8803E5d89aBAd07D;
+contract SimpleInvestment {
+    address payable public owner;
+    mapping(address => uint256) public investments;
 
-    function bet() public payable {
-        require(msg.value > 0, "You need to bet something");
-        balances[msg.sender] += msg.value;
+    constructor() {
+        owner = payable(msg.sender);
     }
 
-    function get() public {
-        require(msg.sender == owner, "Only the owner can withdraw funds");
-        payable(msg.sender).transfer(address(this).balance);
+    function invest() public payable {
+        investments[msg.sender] += msg.value;
     }
 
-    function getBalance() public view returns (uint256) {
-        return balances[msg.sender];
+    function checkInvestment() public view returns (uint256) {
+        return investments[msg.sender];
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only the contract owner can withdraw.");
+        owner.transfer(address(this).balance);
     }
 }
